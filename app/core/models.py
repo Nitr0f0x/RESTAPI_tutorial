7138b1d4134c99d2f3873487b3945893e8788bee
Nitr0F0x <cjt.tuapawa@gmail.com>
2020-11-28 18:17:55 +1030

    added methods and tests for normalization of emails and creating superuser user

--- app/core/models.py
+++ app/core/models.py
@@ -2,19 +2,29 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin 
 
 class UserManager(BaseUserManager):
-    #Creates and saves new users
+    """Creates and saves new users """
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        """ Creates and saves new user """
+        if not email:
+            raise ValueError('Error')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password):
+        """ Creates superuser user """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
     
 
 class User(AbstractBaseUser, PermissionsMixin):
-    #Custom user model that support using email instead of usernames
+    """Custom user model that support using email instead of usernames"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
